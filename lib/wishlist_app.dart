89c2dbea9e3da2f44444728c4b72c62b61022f0f
tukiyubiitai <tukiyubiitai@gmail.com>
2024-02-04 14:25:10 +0900

    lib/wishlist_app.dart

---
+++ lib/wishlist_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class WishlistApp extends ConsumerWidget {
+  const WishlistApp({required this.title,super.key});
+
+   final String title;
+
+  @override
+  Widget build(BuildContext context,WidgetRef ref) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+    );
+  }
+}
